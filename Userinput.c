1c0775935793eeee5d98d1825f0d65d7fc8e4475
WindWorldy <WindWorldy@gmail.com>
2024-08-13 17:38:00 +0200

    Userinput.
    Here is a userinput made from me, C programming language.

---
+++ Userinput.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius;
+    printf("Please enter a radius: ");
+    scanf("%i", &radius);
+
+    printf("The given radius is %i\n",radius);
+    return 0;
+}
